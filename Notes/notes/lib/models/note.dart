568dbaad7d44c56e40529d40e39e2405bd785ff3
abdou-tech-makergit config --global user.name abdou-tech-maker git config --global user.name abdou-tech-maker <abderrahmanekihal5@gmail.com>
2022-09-24 01:49:13 +0200

    commit on 'master '

---
+++ Notes/notes/lib/models/note.dart
@@ -0,0 +1,60 @@
+class Note {
+  int _id;
+  String _title;
+  String _description;
+  String _date;
+  int _priority;
+//Constructor
+  Note(this._title, this._priority, this._date, [this._description]);
+  Note.withId(this._id, this._title, this._priority, this._date,
+      [this._description]);
+// Defining Getters
+  int get id => _id;
+  String get title => _title;
+  String get description => _description;
+  String get date => _date;
+  int get priority => _priority;
+//Defining Setters
+  set title(String newTitle) {
+    if (newTitle.length < 100) {
+      this._title = newTitle;
+    }
+  }
+
+  set description(String newDescription) {
+    this._description = newDescription;
+  }
+
+  set date(String newDate) {
+    this._date = newDate;
+  }
+
+  set priority(int newPriority) {
+    if (newPriority >= 1 && newPriority <= 2) {
+      this._priority = newPriority;
+    }
+  }
+
+//Converting the "Note" Object Into a Map Object In Order To Be Stored In SQFLITE Data Base
+  Map<String, dynamic> toMap() {
+    var map = Map<String, dynamic>();
+    if (_id != null) {
+      map['id'] = _id;
+    }
+    map['title'] = _title;
+    map['decription'] = _description;
+    map['priority'] = _priority;
+    map['date'] = _date;
+
+    return map;
+  }
+
+  //Extract a Note Object From Map object
+  Note.fromMapObject(Map<String, dynamic> map) {
+    this._id = map['id'];
+    this._title = map['title'];
+    this._date = map['date'];
+    this._priority = map['priority'];
+    this._description = map['description'];
+  }
+}
---
+++ Notes/notes/lib/utils/dataBase_Helper.dart
@@ -0,0 +1,91 @@
+import 'package:sqflite/sqflite.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:notes/models/note.dart';
+
+class DataBaseHelper {
+  static DataBaseHelper _dataBaseHelper; // Singelton DataBaseHelper
+  static Database _database; //Singelton database
+
+  String noteTable = 'note_table';
+  String colId = 'id';
+  String colTitle = 'title';
+  String colDescription = 'description';
+  String colPriority = 'priority';
+  String colDate = 'date';
+
+  DataBaseHelper._createInstance(); //Named Contructor to create instance of data base helper
+
+  factory DataBaseHelper() {
+    if (_dataBaseHelper == null) {
+      //create instance of database helper only if it's Null
+
+      _dataBaseHelper =
+          DataBaseHelper._createInstance(); //executed only once "Singelton"
+    }
+    return _dataBaseHelper;
+  }
+
+  Future<Database> get dataBase async {
+    if (_database == null) {
+      _database = await initializeDataBase();
+    }
+    return _database;
+  }
+
+  Future<Database> initializeDataBase() async {
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = directory.path + 'note.db';
+
+    //create or Open the dataBase at a given path
+    var notesDataBase =
+        await openDatabase(path, version: 1, onCreate: _createDb);
+    return notesDataBase;
+  }
+
+  void _createDb(Database db, newVersion) async {
+    await db.execute(
+        'CREATE TABLE $noteTable (£colId INTEGER PRIMARY KEY AUTOINCREMENT ,$colTitle TEXT, $colDescription TEXT , $colPriority INTEGER, $colDate TEXT ) ');
+  }
+
+  //FETCH OPERATION : to Get all the note objects from the database
+  getNoteMapList() async {
+    Database db = await this.dataBase;
+    //var result = await db.rawQuery('SELECT * From $noteTable Order By $colPriority ASC ');
+    var result = await db.query(noteTable, orderBy: '$colPriority ASC');
+    return result;
+  }
+
+  //Insert OPERATION : to insert a note objects to the database
+  Future<int> insertInto(Note note) async {
+    Database db = await this.dataBase;
+    var result = await db.insert(noteTable, note.toMap());
+    return result;
+  }
+
+  //UPDATE OPERATION : to Update a note objects from the database
+  Future<int> updateNote(Note note) async {
+    Database db = await this.dataBase;
+    var result = await db.update(noteTable, note.toMap(),
+        where: '$colId = ?', whereArgs: [note.id]);
+    return result;
+  }
+
+  //DELETE OPERATION : to Delete a note objects from the database
+  Future<int> deleteNote(int id) async {
+    var db = await this.dataBase;
+    int result =
+        await db.rawDelete('DELETE FROM $noteTable WHERE $colId = $id ');
+    return result;
+  }
+
+  //Get number Of Note Objects in Database
+  Future<int> getCount() async {
+    Database db = await this.dataBase;
+    List<Map<String, dynamic>> x =
+        await db.rawQuery('SELECT COUNT (*) from $noteTable ');
+    int result = Sqflite.firstIntValue(x);
+    return result;
+  }
+}
